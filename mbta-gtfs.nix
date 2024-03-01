07d0f5d50c68c804e3a6415554cc461b6231123a
Kayla Firestack <kfirestack@mbta.com>
2024-03-01 08:43:25 -0500

    wip!

---
+++ mbta-gtfs.nix
@@ -0,0 +1,7 @@
+{ fetchzip
+}: fetchzip {
+	name = "MBTA GTFS Static";
+	url = "https://cdn.mbtace.com/archive/20240111.zip";
+	stripRoot = false;
+	hash = "sha256-Nvo740l+eo/vnHzZ0qOIJwsUGrp/4CPDxTpXhVlrLnI=";
+}
